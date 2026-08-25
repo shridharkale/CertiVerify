6b56ee742531c3de41a942faf690d000713d01b4
shridharkale <shridharkale096@gmail.com>
2026-08-25 17:51:40 +0530

    Updates

--- assets/external-link-BhXtiZfS.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./jsx-runtime-fWZbxa4t.js";var t=e(`arrow-left`,[[`path`,{d:`m12 19-7-7 7-7`,key:`1l729n`}],[`path`,{d:`M19 12H5`,key:`x3x0zl`}]]),n=e(`external-link`,[[`path`,{d:`M15 3h6v6`,key:`1q9fwt`}],[`path`,{d:`M10 14 21 3`,key:`gplh6r`}],[`path`,{d:`M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6`,key:`a6xqqp`}]]);export{t as n,n as t};
